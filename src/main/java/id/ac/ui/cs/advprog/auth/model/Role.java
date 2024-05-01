2414113163d82d7c415da1b3197e09edc55842d9
raihanakbr <muhammad.raihan22@ui.ac.id>
2024-05-01 19:37:31 +0700

    [GREEN] Implement Role Model

---
+++ src/main/java/id/ac/ui/cs/advprog/auth/model/Role.java
@@ -0,0 +1,18 @@
+package id.ac.ui.cs.advprog.auth.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+}
