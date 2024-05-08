7bc218d1c7da793405f26ecfc9ab8d8088dd76d2
raihanakbr <muhammad.raihan22@ui.ac.id>
2024-05-08 20:55:02 +0700

    [REFACTOR] Role Model

--- src/main/java/id/ac/ui/cs/advprog/auth/model/Role.java
+++ src/main/java/id/ac/ui/cs/advprog/auth/model/Role.java
@@ -1,18 +1,39 @@
 package id.ac.ui.cs.advprog.auth.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
 
-import javax.persistence.*;
-
-@Setter
-@Getter
 @Entity
 @Table(name = "roles")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
-    private String name;
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role() {
+
+    }
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ERole getName() {
+        return name;
+    }
+
+    public void setName(ERole name) {
+        this.name = name;
+    }
 }
