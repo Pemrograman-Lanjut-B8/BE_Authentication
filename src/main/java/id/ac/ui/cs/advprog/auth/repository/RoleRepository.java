eeba6b32c29746aa6cea8c4c0e8d430fedb37306
raihanakbr <muhammad.raihan22@ui.ac.id>
2024-05-08 20:53:17 +0700

    [REFACTOR] Refactor User Repository to extend JpaRepository

--- src/main/java/id/ac/ui/cs/advprog/auth/repository/RoleRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/auth/repository/RoleRepository.java
@@ -1,12 +1,12 @@
 package id.ac.ui.cs.advprog.auth.repository;
 
-import java.util.Optional;
-
 import id.ac.ui.cs.advprog.auth.model.ERole;
-import id.ac.ui.cs.advprog.auth.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import id.ac.ui.cs.advprog.auth.model.Role;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(ERole name);
--- src/main/java/id/ac/ui/cs/advprog/auth/repository/UserRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/auth/repository/UserRepository.java
@@ -1,56 +1,17 @@
 package id.ac.ui.cs.advprog.auth.repository;
 
-import id.ac.ui.cs.advprog.auth.model.builder.UserBuilder;
-import id.ac.ui.cs.advprog.auth.model.UserEntity;
-import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.UUID;
+import id.ac.ui.cs.advprog.auth.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
 @Repository
-public class UserRepository {
-    private final List<UserEntity> userData = new ArrayList<>();
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsername(String username);
 
-    @Setter
-    @Autowired
-    private UserBuilder userBuilder;
+    Boolean existsByUsername(String username);
 
-    public UserEntity createUser(UserEntity user){
-        userData.add(user);
-        return user;
-    }
-    public Iterator<UserEntity> findAll(){
-        return userData.iterator();
-    }
-    public UserEntity findByUsername(String username){
-        for (UserEntity User: userData){
-            if (User.getUsername().equals(username)){
-                return User;
-            }
-        }
-        return null;
-    }
-
-    public UserEntity update(String username, UserEntity updatedUser){
-        for (int i=0; i<userData.size(); i++){
-            UserEntity user = userData.get(i);
-            if(user.getUsername().equals(username)){
-                UserEntity newUser = userBuilder.reset()
-                        .setCurrent(updatedUser)
-                        .addUsername(updatedUser.getUsername())
-                        .addPassword(updatedUser.getPassword())
-                        .build();
-                userData.remove(i);
-                userData.add(i,newUser);
-                return newUser;
-            }
-        }
-        return null;
-    }
-
+    Boolean existsByEmail(String email);
 }
